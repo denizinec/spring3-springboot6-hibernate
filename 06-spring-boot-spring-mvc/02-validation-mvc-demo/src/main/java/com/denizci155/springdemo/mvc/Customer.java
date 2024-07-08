0c7ee85053314ff885f61703e12448a0b460c0b4
Deniz İneç <denizinec9@gmail.com>
2024-07-08 17:16:50 +0300

    main

---
+++ 06-spring-boot-spring-mvc/02-validation-mvc-demo/src/main/java/com/denizci155/springdemo/mvc/Customer.java
@@ -0,0 +1,53 @@
+package com.denizci155.springdemo.mvc;
+
+import jakarta.validation.constraints.*;
+
+public class Customer {
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    private Integer freePasses;
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String firstName;
+
+    @NotNull(message = "is required")
+    @Size(min =3, message = "is required")
+    public String lastName = "";
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}

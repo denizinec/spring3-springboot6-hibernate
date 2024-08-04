204320c7c6b45c532e53f79496a0bdbb3e43608e
Deniz İneç <denizinec9@gmail.com>
2024-08-04 12:04:11 +0300

    main

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/denizci155/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/denizci155/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import com.denizci155.springboot.thymeleafdemo.entity.Employee;
 import com.denizci155.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -48,12 +45,29 @@ public class EmployeeController {
                 return "employees/employee-form";
         }
 
+        @GetMapping("/showFormUpdate")
+        public String showFormUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+
+                Employee theEmployee = employeeService.findById(theId);
+
+                theModel.addAttribute("employee", theEmployee);
+                return "employees/employee-form";
+        }
+
         @PostMapping("/save")
         public String saveEmployee(@ModelAttribute("employee") Employee theModel) {
 
                 employeeService.save(theModel);
 
             return "redirect:/employees/list";
+        }
+
+        @GetMapping("/delete")
+        public String deleteEmployee(@RequestParam("employeeId") int theId) {
+
+                employeeService.deleteById(theId);
+
+                return "redirect:/employees/list";
         }
 
 

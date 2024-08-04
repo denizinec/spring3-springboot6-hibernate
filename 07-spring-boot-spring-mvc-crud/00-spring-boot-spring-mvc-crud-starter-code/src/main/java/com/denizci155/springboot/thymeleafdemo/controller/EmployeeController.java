581e3097b78fe5f0c74b45f9f1ed0c9874c81751
Deniz İneç <denizinec9@gmail.com>
2024-08-04 10:31:44 +0300

    main

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/denizci155/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/denizci155/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.denizci155.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -35,8 +37,24 @@ public class EmployeeController {
 
                 theModel.addAttribute("employees",theEmployees);
 
-                return "list-employees";
+                return "employees/list-employees";
 
         }
+        @GetMapping("/showFormAdd")
+        public String showFormAdd(Model theModel) {
+
+                Employee theEmployee = new Employee();
+                theModel.addAttribute("employee", theEmployee);
+                return "employees/employee-form";
+        }
+
+        @PostMapping("/save")
+        public String saveEmployee(@ModelAttribute("employee") Employee theModel) {
+
+                employeeService.save(theModel);
+
+            return "redirect:/employees/list";
+        }
+
 
 }
--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/denizci155/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/denizci155/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -2,8 +2,11 @@ package com.denizci155.springboot.thymeleafdemo.dao;
 import com.denizci155.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    // that's it ... no need to write any code LOL!
+    public List<Employee> findAllByOrderByLastNameAsc();
+
 
 }
